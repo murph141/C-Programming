5f8533e4aef04df4ffac77d5a209612c4793891f
Eric Michael Murphy <murph141@ecelinux22.ecn.purdue.edu>
2013-08-20 23:46:04 -0400

    Fixed bash scripts and deleted unnecessary files.

--- PA01/run.sh
+++ PA01/run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+# bash script to call function multiple times
 
+thepath='./inputs/input'
+thepath2='./test/expected'
 for i in {0..19}
 do
-  `./pa01 ./inputs/input$i` #  > ./test/hello$i
+  string=$thepath$i
+  string2=$thepath2$i
+  `./pa01 $string > $string2`
 done
--- PA01/test/test.sh
+++ PA01/test/test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+# file to compare program output to expected solutions
 
 for i in {0..19}
 do
   if diff "./expected$i" "../expected/expected$i"; then
     echo "File $i similar"
   else
-    echo "File $i differ"
+    echo "File $i different"
   fi
 done
