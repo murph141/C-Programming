88b0f31362490130e9d223c6e77c8f60ef7e52ae
Eric Murphy <murph141@purdue.edu>
2013-11-10 12:13:01 -0500

    Starting PA09

--- PA09/pa09.h
+++ PA09/pa09.h
@@ -2,14 +2,14 @@
 #define PA09_H
 
 typedef struct _huffnode {
-    int value; 
-    struct _huffnode * left;
-    struct _huffnode * right;
+  int value; 
+  struct _huffnode * left;
+  struct _huffnode * right;
 } HuffNode;
 
 typedef struct _stack {
-    struct _stack * next;
-    HuffNode * node;
+  struct _stack * next;
+  HuffNode * node;
 } Stack;
 
 #endif
