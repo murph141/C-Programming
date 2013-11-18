f1836c10525d8bb95e75ce1287aaeb7c95cb0f6e
Eric Murphy <murph141@purdue.edu>
2013-11-17 23:09:28 -0500

    Added PA10

--- PA09/pa09.c
+++ PA09/pa09.c
@@ -6,14 +6,16 @@
 
 int main(int argc, char * * argv)
 {
-  int bit;
+  int bit; //Determines if the file is a character file or a binary file
 
+  //Call if three arguments aren't given
   if(argc != 3)
   {
     printf("Usage: ./pa09 <input filename> <output filename>\n");
     return EXIT_FAILURE;
   }
 
+  //Check if the file is a character or binary file
   if(strstr(argv[1], "bit"))
   {
     bit = 1;
@@ -23,24 +25,26 @@ int main(int argc, char * * argv)
     bit = 0;
   }
 
+  //Open the two files
   FILE * fptr = fopen(argv[1], "rb");
   FILE * fwrite = fopen(argv[2], "wb");
 
+  //Call if either of the files aren't opened correctly
   if(fptr == NULL || fwrite == NULL)
   {
     printf("File opened unsuccessfully\n");
     return EXIT_FAILURE;
   }
 
-  HuffNode * tree = NULL;
+  HuffNode * tree = NULL; //Create a new hufftree node
 
-  tree = huff_create(fptr, bit);
+  tree = huff_create(fptr, bit); //Set the tree equal to the output of the huff_create function
 
-  fclose(fptr);
+  fclose(fptr); //Close the input file
 
-  huff_print(tree, fwrite);
+  huff_print(tree, fwrite); //Print the tree contents to the output file
 
-  fclose(fwrite);
+  fclose(fwrite); //Close the output file
 
   return EXIT_SUCCESS;
 }
